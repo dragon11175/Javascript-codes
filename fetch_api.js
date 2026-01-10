59559514d7b71e926deadfe83b961e96c47bebc0
CODEman <lalitsainiak23@gmail.com>
2026-01-10 17:19:06 +0530

    change data into json wit jsoon() function

--- fetch_api.js
+++ fetch_api.js
@@ -1,7 +1,8 @@
 async function fetchuserdata(){
   try{
-    const response = await fetch('https://api.github.com/users/nykz');
-    console.log(response);
+    const response = await fetch('https://api.github.com/users/dragon11175');
+    const result=response.json();
+    console.log(result);
   }catch(e){
     console.error(e);
   }
